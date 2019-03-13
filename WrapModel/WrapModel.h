2ff6e12680c233329057381af23bf71f2e77e572
Ken Worley <ken@1stdibs.com>
2019-03-13 13:33:35 -0400

    include umbrella header

---
+++ WrapModel/WrapModel.h
@@ -0,0 +1,10 @@
+//
+//  WrapModel.h
+//  WrapModelSample
+//
+//  Created by Ken Worley on 3/13/19.
+//  Copyright © 2019 1stdibs. All rights reserved.
+//
+
+#import "WPBoolean.h"
+#import "FDModelSerializing.h"
